1fc58b64a2cb8c8e2c674e6a209b6ebdb206d9ef
Ilham Putra Arysila <ilhamputraaris22@gmail.com>
2024-03-11 19:00:05 +0700

    Tugas Codelab Aquarium

--- app/src/main/java/com/ilhamptra/androidkotlin/Aquarium.kt
+++ app/src/main/java/com/ilhamptra/androidkotlin/Aquarium.kt
@@ -1,17 +1,14 @@
 package com.ilhamptra.androidkotlin
 
-fun main() {
-    fun updateDirty(dirty: Int, operation: (Int) -> Int): Int {
-        return operation(dirty)
+class Aquarium {
+    var width: Int = 20
+    var height: Int = 40
+    var length: Int = 100
+
+    fun printSize() {
+        println("Width: $width cm " +
+                "Length: $length cm " +
+                "Height: $height cm ")
     }
-
-    val waterFilter: (Int) -> Int = { dirty -> dirty / 2 }
-    println(updateDirty(30, waterFilter))
-
-    fun increaseDirty(start: Int) = start + 1
-    println(updateDirty(15, ::increaseDirty))
-
-    var dirtyLevel = 19
-    dirtyLevel = updateDirty(dirtyLevel) { dirtyLevel -> dirtyLevel + 23 }
-    println(dirtyLevel)
 }
+
