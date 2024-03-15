2fe2d8ced7403ff3b94594de1a0f39f0e8a134b6
Ilham Putra Arysila <ilhamputraaris22@gmail.com>
2024-03-15 23:32:31 +0700

    Tugas Codelab Aquarium

--- app/src/main/java/com/ilhamptra/androidkotlin/Main.kt
+++ app/src/main/java/com/ilhamptra/androidkotlin/Main.kt
@@ -1,10 +1,10 @@
 package com.ilhamptra.androidkotlin
 
 fun buildAquarium() {
-    val aquarium6 = Aquarium(numberOfFish = 29)
-    aquarium6.printSize()
-    aquarium6.volume = 70
-    aquarium6.printSize()
+    val myAquarium = Aquarium(width = 25, length = 25, height = 40)
+    myAquarium.printSize()
+    val myTower = Aquarium.TowerTank(diameter = 25, height = 40)
+    myTower.printSize()
 }
 
 fun main() {
