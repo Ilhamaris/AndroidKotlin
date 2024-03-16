89a3e02eaa0d7fb3c7f9cf7e3d5cffa3c415d64c
Ilham Putra Arysila <ilhamputraaris22@gmail.com>
2024-03-16 08:48:22 +0700

    Tugas Codelab Aquarium

--- app/src/main/java/com/ilhamptra/androidkotlin/decor/Decoration.kt
+++ app/src/main/java/com/ilhamptra/androidkotlin/decor/Decoration.kt
@@ -5,7 +5,9 @@ import com.ilhamptra.androidkotlin.makeFish
 // Here is a data class with 3 properties.
 data class Decoration2(val rocks: String, val wood: String, val diver: String){
 }
-
+enum class Color(val rgb: Int) {
+    RED(0xFF0000), GREEN(0x00FF00), BLUE(0x0000FF);
+}
 fun makeDecorations() {
     val d5 = Decoration2("crystal", "wood", "diver")
     println(d5)
@@ -16,6 +18,12 @@ fun makeDecorations() {
     println(wood)
     println(diver)
 }
-fun main () {
-    makeDecorations()
+enum class Direction(val degrees: Int) {
+    NORTH(0), SOUTH(180), EAST(90), WEST(270)
+}
+
+fun main() {
+    println(Direction.EAST.name)
+    println(Direction.EAST.ordinal)
+    println(Direction.EAST.degrees)
 }
